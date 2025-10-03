d350c08515dc6161295f8961d3bb4270f8383110
lesnikovski <lesnikovski@gmail.com>
2025-10-04 00:18:46 +0300

    Handle display No Selected project and new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+import { useState } from 'react';
 import NewProject from './components/NewProject';
+import NoProjectSelected from './components/NoProjectSelected';
 import ProjectSidebar from './components/ProjectSidebar';
 
 function App() {
+  const [projectsState, setProjectstate] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectstate((state) => ({
+      ...state,
+      selectedProjectId: null,
+    }));
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onStartAddProject={handleStartAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NewProject />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
